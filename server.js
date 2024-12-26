6c2264626eb3d5c786a996ea9c5c71cae9ff47ae
devjasani79 <104269899+devjasani79@users.noreply.github.com>
2024-12-26 17:00:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,30 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 connectDb();
+
 const app = express();
-
 const port = process.env.PORT || 5000;
-
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// CORS configuration
+app.use(cors({
+    origin: 'https://mycontactappdev.netlify.app', // Allow your Netlify domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],    // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow necessary headers
+}));
 
 // Correctly mount the contact routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-
-// middleware for error handlers 
+// Middleware for error handlers
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
